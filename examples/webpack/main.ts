1c24992b23630392e34720c8740207ff8f9cc751
rahulvig-trepp <100596264+rahulvig-trepp@users.noreply.github.com>
2022-12-07 14:09:32 -0500

    working

--- examples/webpack/main.ts
+++ examples/webpack/main.ts
@@ -1,6 +1,8 @@
-import 'html2canvas';
+import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
 import 'jspdf-autotable';
+import { jsPDFConstructor, jsPDFDocument } from 'jspdf-autotable';
+import { opensans } from './raw_opensans';
 
 const pdf = new jsPDF('p', 'pt', 'letter')
 
@@ -73,6 +75,7 @@ const getPaginatedTabsHTML = (classSelectors: Record<string, string>, pdfOutput:
     const tabs: HTMLElement[] = Array.from(pdfOutput.querySelectorAll(`.${tabClass}`));
     return tabs.reduce((paginatedTabsHTML: string[], currTab: HTMLElement, currTabIdx: number) => {
         const currTabClass = currTab.className;
+        const image = currTab.querySelector('img');
         paginatedTabsHTML.push(`
             <div class='${currTabClass}'>
                 <div class='${tabHeaderClass}'>
@@ -85,11 +88,164 @@ const getPaginatedTabsHTML = (classSelectors: Record<string, string>, pdfOutput:
                     ${currTab.querySelector(`.${addressClass}`).outerHTML}
                 </div>
                 ${currTab.querySelector(`.${tabTitleClass}`).outerHTML}
+                ${image ? (`<img src=${image.src} width='180px' height='180px' />`) : ''}
                 ${currTab.querySelector(`.${tabTablesClass}`).outerHTML}
             </div>
         `.trim())
         return paginatedTabsHTML;
     }, [])
+}
+
+
+type PageNumber = number
+
+type TablePageMeta = Map<string, PageNumber>;
+type LoanDetailsPDFTableOfContents = Map<string, TablePageMeta>
+
+
+const getTableOfContents = (classSelectors: any, pdfOutput: HTMLElement): LoanDetailsPDFTableOfContents[] => {
+    const { tabClass, tableWrapperClass, detailsRightClass, tableTitleClass } = classSelectors;
+    const maxPageHeight = 780;
+    let currPageHeight = 0;
+    const tocPagesMeta: LoanDetailsPDFTableOfContents[] = []
+    let tableOfContents: LoanDetailsPDFTableOfContents = new Map();
+    const tabPages = Array.from(pdfOutput.querySelectorAll(`.${tabClass}`));
+    for (const tabPage of tabPages) {
+        currPageHeight += 30
+        //if currPageHeight is exceeded here, create new toc page with currTab and all following tabs in new tocPage
+        if (currPageHeight > maxPageHeight) {
+            tocPagesMeta.push(tableOfContents);
+            tableOfContents = new Map() as LoanDetailsPDFTableOfContents;
+            currPageHeight = 0;
+        }
+        const tabName = tabPage.classList[0].split(/(?=[A-Z])/).join(' ');
+        const pageNum = Number(tabPage.querySelector(`.${detailsRightClass}`).textContent.split('. ')[1])
+        const tabTables = Array.from(tabPage.querySelectorAll(`.${tableWrapperClass}`))
+        if (!tableOfContents.get(tabName)) {
+            tableOfContents.set(tabName, new Map() as TablePageMeta)
+        }
+        for (const tabTable of tabTables) {
+            currPageHeight += 20;
+            //if currPageHeight is exceeded here, create new toc page with currTab and remaining tables along with all consequent tabs being added to new page
+            if (currPageHeight > maxPageHeight) {
+                tocPagesMeta.push(tableOfContents)
+                tableOfContents = new Map();
+                tableOfContents.set(tabName, new Map() as TablePageMeta)
+                currPageHeight = 0;
+            }
+            const tableName = formatText(tabTable.querySelector(`.${tableTitleClass}`).textContent);
+            if (!tableOfContents.get(tabName).get(tableName)) {
+                tableOfContents.get(tabName).set(tableName, pageNum);
+            }
+        }
+    }
+    tocPagesMeta.push(tableOfContents);
+    return tocPagesMeta;
+}
+
+const addTableOfContentsToPDF = (tocByPage: LoanDetailsPDFTableOfContents[], pdf: jsPDF) => {
+    // pdf.textWithLink(text, 20, y, { pageNumber: i });
+    const textOptions = { charSpace: 0 }
+    let pageBuffer = tocByPage.length;
+    tocByPage.forEach((toc, tocIdx) => {
+        pdf.addPage();
+        let currX = 50
+        let currY = 50;
+        pdf.setFontSize(20);
+        pdf.setFont('helvetica', 'bold');
+        pdf.text(`${tocIdx === 0 ? 'Table of Contents' : 'Table of Contents (cont.)'}`, currX, currY, textOptions);
+        currX += 20;
+        currY += 30;
+        toc.forEach((sectionInfo, sectionName) => {
+            pdf.setFontSize(11);
+            pdf.setFont('helvetica', 'bold');
+            pdf.setTextColor('#2C5CAA');
+            pdf.text(sectionName, currX, currY, textOptions);
+            currY += 20;
+            pdf.setFontSize(9);
+            pdf.setFont('helvetica', 'normal');
+            sectionInfo.forEach((tablePage, tableName) => {
+                let dots = '.'
+                let text = `${tableName} ${dots} ${`pg. ${tablePage}`}`
+                while (Math.floor(pdf.getTextWidth(text)) < 470) {
+                    dots = `${dots}.`
+                    text = `${tableName} ${dots} ${`pg. ${tablePage}`}`
+                }
+                console.log('textWidth', pdf.getTextWidth(text));
+                console.log('text.length', text.length)
+                pdf.setTextColor('black')
+                pdf.textWithLink(text, currX + 10, currY, { pageNumber: tablePage + pageBuffer + tocIdx + 1 })
+                currY += 20;
+            })
+            currY += 10
+        })
+        pageBuffer -= 1;
+        pdf.movePage(pdf.getNumberOfPages(), tocIdx + 2)
+    })
+}
+
+
+export type CoverPageOptions = {
+    backgroundImageSrc?: string;
+    logoSrc?: string;
+    textColor: string;
+    textOverlayColor: string;
+    textOverlayTransparency: number;
+    reportTitle: string;
+    reportDetails?: string[];
+}
+
+export const coverPageOpts: CoverPageOptions = {
+    backgroundImageSrc: 'city.png',
+    logoSrc: 'trepp-logo.png',
+    textColor: '#000000',
+    textOverlayColor: '#FFFFFF',
+    textOverlayTransparency: 0.8,
+    reportTitle: 'Details Report',
+    reportDetails: ['One Vanderbilt', 'New York-Newark-Jersey City, NY-NJ-PA', 'Office'],
+
+
+}
+
+const addCoverPage = async (pdf: jsPDFDocument, coverPageOptions: CoverPageOptions, pdfOptions: any) => {
+    const { pageWidth = 612, pageHeight = 792 } = pdfOptions;
+    const { backgroundImageSrc, logoSrc, textOverlayColor, textColor, textOverlayTransparency, reportTitle, reportDetails } = coverPageOptions;
+    await pdf.addPage();
+    const html = (`
+        <div>
+            <img src='${backgroundImageSrc}' style='position: absolute; z-index: 0;'/>
+        </div>
+    `);
+    await pdf.html(html, {
+        autoPaging: false,
+        width: pageWidth,
+        windowWidth: pageWidth,
+        html2canvas: {
+            async: true,
+            height: pageHeight,
+            windowHeight: pageHeight,
+            useCORS: true,
+        }
+    })
+    pdf.saveGraphicsState();
+    pdf.setGState(new pdf.GState({ opacity: 0.8 }));
+    const reportTitleDimensions = pdf.getTextDimensions(reportTitle);
+    const reportDetailsDimensions = pdf.getTextDimensions(reportDetails);
+    console.log('reportDetailsDimensions', reportDetailsDimensions)
+
+    pdf.setFillColor(textOverlayColor);
+    pdf.rect(0, 300, 612, 200, 'F');
+    pdf.restoreGraphicsState();
+    pdf.addImage(logoSrc, (pageWidth - 150) / 2, pageHeight / 2.5, 150, 50);
+    pdf.setFontSize(25);
+    pdf.setFont('helvetica', 'bold')
+    pdf.setTextColor(textColor)
+    pdf.text(reportTitle, (pageWidth - pdf.getTextWidth(reportTitle)) / 2, pageHeight / 2);
+    pdf.setFont('helvetica', 'bold');
+    pdf.setFontSize(13);
+    const splitReportDetails = reportDetails.reduce((acc, details) => acc.concat(...pdf.splitTextToSize(details, 600)), [])
+    pdf.text(splitReportDetails, pageWidth / 2 - (reportDetailsDimensions.w), pageHeight / 2 + 50);
+    await pdf.movePage(pdf.getNumberOfPages(), 1)
 }
 
 const generatePDF = async ({ pdfOptions, classSelectors }: any) => {
@@ -104,7 +260,13 @@ const generatePDF = async ({ pdfOptions, classSelectors }: any) => {
         const tabTables: HTMLDivElement = tab.querySelector(`.${tabTablesClass}`);
         handleOverflow({ classSelectors, tab, pageHeader: tabHeader, pageName: tabTitle.textContent, tabTables });
     }
+
     const paginatedTabsHTML: string[] = getPaginatedTabsHTML(classSelectors, pdfOutput);
+    pdfOutput.innerHTML = paginatedTabsHTML.map(tabHTML => tabHTML).join('')
+    console.log('pdfOutput.innerHTML', pdfOutput.innerHTML)
+    const tableOfContentsPerPage: LoanDetailsPDFTableOfContents[] = getTableOfContents(classSelectors, pdfOutput);
+
+
     for (let tabIdx in paginatedTabsHTML) {
         await pdf.html(paginatedTabsHTML[tabIdx], {
             autoPaging: false,
@@ -113,13 +275,18 @@ const generatePDF = async ({ pdfOptions, classSelectors }: any) => {
             html2canvas: {
                 async: true,
                 height: pageHeight - 1,
-                windowHeight: pageHeight
+                windowHeight: pageHeight,
+                useCORS: true,
             }
         });
-        if (Number(tabIdx) < paginatedTabsHTML?.length - 1) {
+        if (Number(tabIdx) < paginatedTabsHTML.length - 1) {
             pdf.addPage();
         }
     }
+
+    const imageSrc = './city.png';
+    await addCoverPage(pdf, coverPageOpts, pdfOptions);
+    addTableOfContentsToPDF(tableOfContentsPerPage, pdf);
     pdf.save();
 }
 
@@ -133,7 +300,8 @@ const handleOverflow = (
     const tabTables = getLatestTabTables(classSelectors, tab);
     let { wideTablesHandled, longTablesHandled } = overflowValidations;
     let { clientWidth: tabTablesClientWidth, clientHeight: tabTablesClientHeight, scrollWidth: tabTablesScrollWidth, scrollHeight: tabTablesScrollHeight } = tabTables;
-    // No overflow exists and no DOM Manipulation needed, skip!
+
+    // No image and no overflow exists and no DOM Manipulation needed, skip!
     if (tabTablesClientWidth === tabTablesScrollWidth && tabTablesClientHeight === tabTablesScrollHeight) {
         return;
     }
@@ -191,7 +359,7 @@ const splitWideTable = (classSelectors: Record<string, string>, tableWrapper: HT
     const tableBodyRows: HTMLTableRowElement[] = Array.from(tableBody.rows);
     const spliceIdx = Array.from(tableBodyRows[0].cells).findIndex(((cell) => cell.getBoundingClientRect().right > tabTablesRight));
     const splitTableHeadCells: SplitTableCell[][][] = getSplitTableHeadCells(tableHeadRows, spliceIdx, tabTablesRight);
-    const splitTableBodyCells: SplitTableCell[][][] = getSplitTableBodyCells(tableBodyRows, spliceIdx, splitTableHeadCells, tabTablesRight);
+    const splitTableBodyCells: SplitTableCell[][][] = getSplitTableBodyCells(tableBodyRows, spliceIdx, splitTableHeadCells);
     return buildSplitWideTablesHTML(classSelectors, { tableWrapperClass: tableFullClass, tableHeader, splitTableHeadCells, splitTableBodyCells })
 
 }
@@ -264,6 +432,7 @@ const buildSplitWideTablesHTML = (classSelectors: Record<string, string>, { tabl
     const splitTableHeadHTML: string[][] = buildTHeadHTML(splitTableHeadRows);
     const firstTableBodyHTML: string[][] = buildTBodyHTML(firstTableBodyRows);
     const splitTableBodyHTML: string[][] = buildTBodyHTML(splitTableBodyRows);
+    // TODO: We need to pass a max width and split the wide table further if needed
     const splitTablesHTML = [
         `<div class='${tableWrapperClass}'>
             <div class='${tableContainerClass}'>
@@ -318,27 +487,29 @@ const splitLongTables = ({ classSelectors, tab, pageHeader, pageName, tabTables 
     const { bottom: tabTablesBottom } = tabTables.getBoundingClientRect();
     const tableWrappers: HTMLElement[] = Array.from(tabTables.getElementsByClassName(`${tableWrapperClass}`)) as HTMLElement[];
     const updatedHTMLByTable: string[] = []
+    const image = tab.querySelector('img');
     for (const tableWrapper of tableWrappers) {
         const { bottom, height } = tableWrapper.getBoundingClientRect();
         if (bottom > tabTablesBottom) {
-            const splitTableHTML: string[] = splitLongTable(classSelectors, tableWrapper, { tableBottom: bottom, tableHeight: height, tabTablesMaxHeight: tabTables.clientHeight, tabTablesBottom });
+            const splitTableHTML: string[] = splitLongTable(classSelectors, tableWrapper, tab, { tableBottom: bottom, tableHeight: height, tabTablesMaxHeight: tabTables.clientHeight, tabTablesBottom });
             updatedHTMLByTable.push(...splitTableHTML);
         } else {
             updatedHTMLByTable.push(tableWrapper.outerHTML)
         }
     }
-    tabTables.innerHTML = updatedHTMLByTable.join('')
+    tabTables.innerHTML = image ? `<img src='${image.src}'/>${updatedHTMLByTable.join('')}` : updatedHTMLByTable.join('');
     handleOverflow({ classSelectors, tab, pageHeader, pageName, tabTables }, { wideTablesHandled: true, longTablesHandled: true });
 }
 
-const splitLongTable = (classSelectors: Record<string, string>, tableWrapper: HTMLElement, { tabTablesMaxHeight, tabTablesBottom }: Boundaries): string[] => {
+const splitLongTable = (classSelectors: Record<string, string>, tableWrapper: HTMLElement, tab: any, { tabTablesMaxHeight, tabTablesBottom }: Boundaries): string[] => {
     const { tableContainerClass, tableTitleClass, tableClass } = classSelectors;
     const tableFullClass: string = tableWrapper.className;
     const tableContainer: HTMLElement = tableWrapper.querySelector(`.${tableContainerClass}`);
     const tableHeader: HTMLElement = tableContainer.querySelector(`.${tableTitleClass}`);
     const table: HTMLTableElement = tableContainer.querySelector(`.${tableClass}`);
     const tableHead: HTMLTableSectionElement = table.tHead;
-    const maxRowHeight: number = tabTablesMaxHeight - tableHeader.clientHeight - tableHead.clientHeight;
+    const image = tab.querySelector('img');
+    const maxRowHeight: number = tabTablesMaxHeight - tableHeader.clientHeight - tableHead.clientHeight
     const tableBody: HTMLTableSectionElement = table.tBodies[0];
     const tableBodyRows: HTMLTableRowElement[] = Array.from(tableBody.rows);
     // Find the first row whose bottom exceeds tabTablesBottom. This is the row that we need to start the split from
@@ -392,14 +563,15 @@ const buildSplitLongTablesHTML = (classSelectors: Record<string, string>, { tabl
 
 const buildAdditionalTabPages = ({ classSelectors, tab, tabTables }: TabOverflowMeta) => {
     const { tableWrapperClass } = classSelectors
-    const { right: tabTablesRight } = tabTables.getBoundingClientRect();
+    const { bottom: tabBottom } = tab.getBoundingClientRect();
+    const { right: tabTablesRight, bottom: tabTablesBottom } = tabTables.getBoundingClientRect();
     const tableWrappers: HTMLElement[] = Array.from(tabTables.getElementsByClassName(`${tableWrapperClass}`)) as HTMLElement[];
     //Find the index of the first horizontal overflowing table;
     let spliceIdx = -1;
     for (const tableWrapperIdx in tableWrappers) {
         const tableWrapper = tableWrappers[tableWrapperIdx];
-        const { right } = tableWrapper.getBoundingClientRect();
-        if (right > tabTablesRight) {
+        const { right, bottom } = tableWrapper.getBoundingClientRect();
+        if (right > tabTablesRight || bottom > tabBottom) {
             spliceIdx = Number(tableWrapperIdx);
             break;
         }
@@ -436,7 +608,8 @@ const buildSplitTabTablesHTML = (classSelectors: Record<string, string>, tab: HT
     const msaText = formatText(tab.querySelector(`.${detailsLeftClass}`).textContent)
     const addressText = formatText(tab.querySelector(`.${addressClass}`).textContent);
     const titleText = tab.querySelector(`.${tabTitleClass}`).textContent.trim();
-    //We need to break down the original x overflowing tab into one or more additional tabPages
+    //We need to break down the original x overflowing tab into one or more additional tabPages        
+    const image = tab.querySelector('img');
     return tabTablesPerPage.map((pageTables, pageIdx) => {
         return `
             <div class='${tab.className} ${pageIdx > 0 ? `continued-${pageIdx}` : ''}'>
@@ -453,9 +626,10 @@ const buildSplitTabTablesHTML = (classSelectors: Record<string, string>, tab: HT
                         ${addressText}
                     </div>
                 </div>
-                <div class='${tabTitleClass}'>
-                    ${titleText} ${pageIdx > 0 ? `(cont.)` : ''}
+                <div class='${tabTitleClass}'>               
+                ${titleText} ${pageIdx > 0 ? `(cont.)` : ''}
                 </div>
+                ${(pageIdx === 0 && image ? `<img src=${image.src} width='200px' height='200px' />` : '')}
                 <div class='${tabTablesClass}'>
                     ${pageTables.map(table => table.outerHTML).join('')}
                 </div>
@@ -465,3 +639,5 @@ const buildSplitTabTablesHTML = (classSelectors: Record<string, string>, tab: HT
 }
 
 generatePDF({ pdfOptions, classSelectors });
+
+// src="http://www.google.com/maps/api/staticmap?center=47.67681,-122.09707&zoom=14&size=200x200&sensor=false&maptype=roadmap&format=png&markers=size:small|label:S|color:0x932a8e|47.6768,-122.0971&client=gme-trepp&signature=MXy7QCE93qdbPC1Gt1CT61LJ9Ow="
